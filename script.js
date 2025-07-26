99b025236b37033caae827ed34209cf8405931d7
Ekta-pawar <ektapawar0001@gmail.com>
2025-07-26 13:12:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
-    //Write your code here
+  const form = document.querySelector("form1"); // Corrected ID spelling
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
 
+    const fname = document.querySelector(".firstname").value;
+    const lname = document.querySelector(".lastname").value;
+
+    const fullname = `${fname}  ${lname}`; // Added space between names
+    alert(`Full name = ${fullname}`);
+  });
 }
